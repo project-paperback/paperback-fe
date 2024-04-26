8481fb463915ccd7a897159f8f05c461f0f217fe
Riccardo Foti <riccardofoti97@gmail.com>
2024-04-26 10:18:17 +0100

    Make navbar responsive

--- src/components/nav/NavLinks.jsx
+++ src/components/nav/NavLinks.jsx
@@ -3,11 +3,13 @@ import { IconProfile, ShoppingCartOutline } from "../icons/Icons";
 
 const NavigationLinks = () => {
   return (
-    <div className="text-lg flex items-center *:flex *:gap gap-4 *:text-[#023047] *:transition-all *:ease-in *:duration-300 w-fit h-full ">
-      <NavLink className="hover:opacity-[0.7]">Bookstore</NavLink>
-      <NavLink className="hover:opacity-[0.7]">Contact</NavLink>
-      <NavLink className="hover:opacity-[0.7]">Sign Up</NavLink>
-      <div className="flex items-center *:ease-in *:duration-300 gap-[3px]">
+    <div className="text-lg flex items-center gap-4 *:text-[#023047] *:transition-all *:ease-in *:duration-300 w-fit h-full">
+      <NavLink className="hover:opacity-[0.7] hidden lg:block">
+        Bookstore
+      </NavLink>
+      <NavLink className="hover:opacity-[0.7] hidden lg:block">Contact</NavLink>
+      <NavLink className="hover:opacity-[0.7] hidden lg:block">Sign Up</NavLink>
+      <div className="items-center *:ease-in *:duration-300 gap-[6px] hidden lg:inline-flex">
         <IconProfile />
         <NavLink className="hover:opacity-[0.7]">Sign In</NavLink>
       </div>
