6284f933696f87b8cdd12639130e84f996975ba0
Alfredo <alfredo.galvez1996@gmail.com>
2024-05-02 14:10:42 +0100

    add updates

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -9,6 +9,25 @@ export function BookDetails() {
     book_id,
     pageNumber
   );
-  console.log(data);
-  return <div>hello</div>;
+  return (
+    <div>
+      {isPending ? (
+        <p>Loading book details...</p>
+      ) : (
+        <div className="container bg-cyan-200 flex gap-6">
+          <div className="book-detail-img bg-gray-100 w-[40%] px-[5rem] py-[1rem] ">
+            <img
+              src={data.book.imageLinks[0]}
+              alt=""
+              className="mx-[1rem] my-[1rem] shadow-xl shadow-gray-400"
+            />
+          </div>
+          <div className="bg-red-200 w-full">
+            <h1 className="text-2xl">{data.book.title}</h1>
+            <p>£{data.book.price.toFixed(2)}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
