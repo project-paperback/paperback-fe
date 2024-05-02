7d01b3745c19336df7a5ff34603a55aa358b30bd
Alfredo <alfredo.galvez1996@gmail.com>
2024-05-02 11:33:57 +0100

    Updates -Update router to create router browser -Create book details page

--- src/components/nav/NavLinks.jsx
+++ src/components/nav/NavLinks.jsx
@@ -3,17 +3,23 @@ import { IconProfile, ShoppingCartOutline } from "../Icons";
 
 const NavigationLinks = () => {
   return (
-    <div className="text-lg flex items-center gap-4 *:text-[#023047] *:transition-all *:ease-in *:duration-300 w-fit h-full">
-      <NavLink className="hover:opacity-[0.7] hidden lg:block">
+    <div className="text-lg flex items-center gap-4 *:text-[#023047] *:transition-all *:ease-in *:duration-300 w-fit h-full nav-links">
+      <NavLink className={`hover:opacity-[0.7] hidden lg:block`} to="bookstore">
         Bookstore
       </NavLink>
-      <NavLink className="hover:opacity-[0.7] hidden lg:block">Contact</NavLink>
-      <NavLink className="hover:opacity-[0.7] hidden lg:block">Sign Up</NavLink>
+      <NavLink className="hover:opacity-[0.7]  hidden lg:block" to="contact">
+        Contact
+      </NavLink>
+      <NavLink className="hover:opacity-[0.7] hidden lg:block" to="sign_up">
+        Sign Up
+      </NavLink>
       <div className="items-center *:ease-in *:duration-300 gap-[6px] hidden lg:inline-flex">
         <IconProfile />
-        <NavLink className="hover:opacity-[0.7]">Sign In</NavLink>
+        <NavLink className="hover:opacity-[0.7]" to="sign_in">
+          Sign In
+        </NavLink>
       </div>
-      <NavLink className="hover:opacity-[0.7]">
+      <NavLink className="hover:opacity-[0.7]" to="shopping_cart">
         <ShoppingCartOutline height={"1.8rem"} width={"1.8rem"} />
       </NavLink>
     </div>
