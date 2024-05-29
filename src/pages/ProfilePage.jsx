a722646ec962e96f4c872002cb614fe0ad35d3ac
Riccardo Foti <riccardofoti97@gmail.com>
2024-05-29 17:33:01 +0100

    Start building profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,17 +1,77 @@
 import axios from "axios";
+import { InputField } from "../components/SmallComponents";
 
 const ProfilePage = () => {
   return (
-    <button
-      onClick={async () => {
-        await axios.delete(
-          "https://paperback-vy73.onrender.com/api/delete_account"
-        );
-        localStorage.clear();
-      }}
-    >
-      Delete User
-    </button>
+    <div className="2xl:w-[1100px] lg:w-[80%] m-auto px-10">
+      <div className="flex items-center">
+        <img
+          src="https://t4.ftcdn.net/jpg/05/09/59/75/360_F_509597532_RKUuYsERhODmkxkZd82pSHnFtDAtgbzJ.jpg"
+          alt=""
+          className="w-[10rem] sm:w-[15rem]"
+        />
+        <div className="leading-4">
+          <InputField
+            autoComplete={"off"}
+            placeholder={"Name"}
+            type={"text"}
+            className={
+              "placeholder:text-[#023047] bg-gray-200 p-2 outline-none"
+            }
+          />{" "}
+          <br />
+          <InputField
+            autoComplete={"off"}
+            placeholder={"Surname"}
+            type={"text"}
+            className={
+              "placeholder:text-[#023047] bg-gray-200 p-2 outline-none"
+            }
+          />
+        </div>
+      </div>
+      <div className="border-t-[2px] border-[#023047] pt-6">
+        <h2 className="text-[2rem]">Account Management</h2>
+        <form className="py-10">
+          <InputField
+            autoComplete={"off"}
+            placeholder={"Email"}
+            type={"email"}
+            className={
+              "placeholder:text-[#023047] bg-gray-200 p-2 w-[50%] outline-none"
+            }
+          />{" "}
+          <br />
+          <InputField
+            autoComplete={"off"}
+            placeholder={"Password"}
+            type={"password"}
+            className={
+              "placeholder:text-[#023047] bg-gray-200 p-2 w-[50%] outline-none"
+            }
+          />
+          <p>
+            <button className="bg-[#023047] text-white p-3 mt-6">
+              Save Changes
+            </button>
+          </p>
+        </form>
+      </div>
+      <div className="border-t-[2px] border-[#023047] pt-6">
+        <h2 className="text-[2rem]">Danger Zone</h2>
+        <button
+          className="bg-[#023047] p-3 text-red-500 mt-6"
+          onClick={async () => {
+            await axios.delete(
+              "https://paperback-vy73.onrender.com/api/delete_account"
+            );
+            localStorage.clear();
+          }}
+        >
+          Delete Your Account
+        </button>
+      </div>
+    </div>
   );
 };
 export default ProfilePage;
