f8e872319b9e4c8f1d8aeaae5dddbae8cf6a9bf1
Alfredo <alfredo.galvez1996@gmail.com>
2024-06-04 23:04:05 +0100

    updates - Add filter component

---
+++ src/components/BookFilter.jsx
@@ -0,0 +1,220 @@
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+export function Filters() {
+  const [selectedPublishers, setSelectedPublishers] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handlePublisherCheckboxChange = (e) => {
+    const value = e.target.value;
+    setSelectedPublishers((prevSelected) =>
+      e.target.checked
+        ? [...prevSelected, value]
+        : prevSelected.filter((publisher) => publisher !== value)
+    );
+  };
+
+  const handleCategoryCheckboxChange = (e) => {
+    const value = e.target.value;
+
+    setSelectedCategories((prevSelected) =>
+      e.target.checked
+        ? [...prevSelected, value]
+        : prevSelected.filter((category) => category !== value)
+    );
+  };
+
+  useEffect(() => {
+    const params = Object.fromEntries([...searchParams]);
+
+    console.log(params);
+    if (selectedPublishers.length > 0) {
+      params.publisher = selectedPublishers.join(",");
+      console.log(params);
+    } else {
+      delete params.publisher; // Remove publisher param if none selected
+    }
+    if (selectedCategories.length > 0) {
+      params.category = selectedCategories.join(",");
+    } else {
+      delete params.category; // Remove category param if none selected
+    }
+
+    setSearchParams(params);
+    console.log(searchParams);
+  }, [selectedPublishers, selectedCategories, searchParams]);
+
+  return (
+    <form className="mr-8 flex flex-col gap-2">
+      <h2 className="roboto-regular border-[#778da9] border-b-[1px] pb-2 text-2xl">
+        Filter by
+      </h2>
+      {/* publisher filters */}
+      <div className="collapse rounded-none border-[#778da9] border-b-[1px]">
+        <input type="checkbox" />
+        <div className="collapse-title text-[1rem] font-medium">
+          <h2 className="roboto-regular">Publisher</h2>
+        </div>
+        <div className="collapse-content flex flex-col gap-3">
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="cambridge"
+              value="Cambridge University Press"
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handlePublisherCheckboxChange}
+            />
+            <label htmlFor="cambridge">Cambridge University Press</label>
+          </div>
+
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="grove"
+              value="Grove Press"
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handlePublisherCheckboxChange}
+            />
+            <label htmlFor="grove">Grove Press</label>
+          </div>
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="oxford"
+              value="Oxford University Press"
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handlePublisherCheckboxChange}
+            />
+            <label htmlFor="oxford">Oxford University Press</label>
+          </div>
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="penguin"
+              value={"Penguin"}
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handlePublisherCheckboxChange}
+            />
+            <label htmlFor="penguin">Penguin</label>
+          </div>
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="routledge"
+              value={"Routledge"}
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9]   [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handlePublisherCheckboxChange}
+            />
+            <label htmlFor="routledge">Routledge</label>
+          </div>
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="wordsworth"
+              value={"Wordsworth Editions"}
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handlePublisherCheckboxChange}
+            />
+            <label htmlFor="wordsworth">Wordsworth Editions</label>
+          </div>
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="yale"
+              value={"Yale University Press"}
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handlePublisherCheckboxChange}
+            />
+            <label htmlFor="yale">Yale University Press</label>
+          </div>
+        </div>
+      </div>
+      {/* publisher filters end */}
+      {/* category filters */}
+      <div className="collapse rounded-none border-[#778da9] border-b-[1px]">
+        <input type="checkbox" />
+        <div className="collapse-title text-[1rem] font-medium">
+          <h2 className="roboto-regular">Category</h2>
+        </div>
+
+        <div className="collapse-content flex flex-col gap-3">
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="architecture"
+              value="Architecture"
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handleCategoryCheckboxChange}
+            />
+            <label htmlFor="architecture"> Architecture</label>
+          </div>
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="art"
+              value="Art"
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handleCategoryCheckboxChange}
+            />
+            <label htmlFor="art"> Art</label>
+          </div>
+
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="education"
+              value="Education"
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handleCategoryCheckboxChange}
+            />
+            <label htmlFor="education">Education</label>
+          </div>
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="fiction"
+              value="Fiction"
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handleCategoryCheckboxChange}
+            />
+            <label htmlFor="fiction">Fiction</label>
+          </div>
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="history"
+              value="History"
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handleCategoryCheckboxChange}
+            />
+            <label htmlFor="history">History</label>
+          </div>
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="nonfiction"
+              value="Non-Fiction"
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handleCategoryCheckboxChange}
+            />
+            <label htmlFor="nonfiction">Non-Fiction</label>
+          </div>
+
+          <div className="flex flex-row">
+            <input
+              type="checkbox"
+              id="psychology"
+              value="Psychology"
+              className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
+              onChange={handleCategoryCheckboxChange}
+            />
+            <label htmlFor="psychology">Psychology</label>
+          </div>
+        </div>
+      </div>
+      {/* category filters end*/}
+    </form>
+  );
+}
--- src/pages/BookstorePage.jsx
+++ src/pages/BookstorePage.jsx
@@ -1,14 +1,17 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
+import { Navigate, useSearchParams } from "react-router-dom";
+// components
 import { usePagination } from "../utilities_&_custom_hooks/General";
 import { IconArrowLeftCircle, IconArrowRightCircle } from "../components/Icons";
 import BookTile from "../components/bookstore/BookTile";
-
+import { BasketWarningModal } from "../components/SmallComponents";
+import { Filters } from "../components/BookFilter";
+// Custom hooks
 import { useFetchData } from "../utilities_&_custom_hooks/fetchHooks";
 import { useSendToBasket } from "../utilities_&_custom_hooks/postLogs";
-import { BasketWarningModal } from "../components/SmallComponents";
-import { Link, Navigate } from "react-router-dom";
 
 export function BookstorePage() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const topRef = useRef(null);
 
   const scrollToTop = () => {
@@ -23,8 +26,10 @@ export function BookstorePage() {
   const { data, isPending, error } = useFetchData(
     "https://paperback-vy73.onrender.com/api/books",
     id,
-    currentPage
+    currentPage,
+    searchParams
   );
+
   useEffect(() => {
     let timer;
 
@@ -44,13 +49,64 @@ export function BookstorePage() {
   }, [isPending]);
   const { itemSent, errorInBasket, sendToBasket, setErrorInBasket } =
     useSendToBasket();
+  const hidden = data.msg === "More books coming soon!" ? "hidden" : "block";
+  return (
+    <div className="relative">
+      {errorInBasket && (
+        <BasketWarningModal
+          setErrorInBasket={setErrorInBasket}
+          msg={errorInBasket.response?.data?.msg}
+        />
+      )}
 
-  if (!data) {
-    return (
-      <div className="flex flex-col">
-        <div>No books</div>
-        <div className="flex justify-center items-center bg-red-300 h-9">
-          <div className="flex  gap-6">
+      <h2
+        className="text-[2.5rem] text-center my-[2rem] font-bold hidden lg:block "
+        ref={topRef}
+      >
+        <span className="text-[1.2rem]">Our </span>
+        <br />
+        BOOKSTORE
+      </h2>
+
+      <div className="flex relative">
+        <div className="gallery-grid justify-center ">
+          {data.msg === "More books coming soon!" && (
+            <div className="w-[100%] h-[100%] my-auto  absolute flex flex-col justify-center items-center text-[2rem]">
+              <p>{data.msg}</p>
+            </div>
+          )}
+          <div className="grid-filter">
+            <Filters />
+          </div>
+          {isPending ? (
+            <div className="absolute top-0 bottom-0  w-full h-[80vh] bg-white flex justify-center content-center">
+              <span className="loading loading-spinner loading-lg "></span>
+            </div>
+          ) : data?.books ? (
+            data.books.books.map((book) => (
+              <BookTile
+                title={book.title}
+                publisher={book.publisher}
+                imageLinks={book.imageLinks}
+                authors={book.authors}
+                price={book.price}
+                key={book._id}
+                bookId={book._id}
+                sendToBasket={sendToBasket}
+              />
+            ))
+          ) : (
+            <p></p>
+          )}
+        </div>
+      </div>
+      <div className={`flex justify-center items-center  h-9 py-7 ${hidden}`}>
+        <div className="flex  gap-4">
+          {currentPage === 1 ? (
+            <button disabled={true} className="hidden">
+              <IconArrowLeftCircle className={"h-7 w-7"} />
+            </button>
+          ) : (
             <button
               onClick={(e) => {
                 handlePrevPage();
@@ -59,155 +115,32 @@ export function BookstorePage() {
             >
               <IconArrowLeftCircle className={"h-7 w-7"} />
             </button>
-            <p>Page {currentPage}</p>
-            <button disabled={true}>
-              {" "}
+          )}
+
+          <p>Page {currentPage}</p>
+
+          {currentPage === Math.ceil(data?.books?.lengthOfCollection / 12) ? (
+            <button
+              className="hidden"
+              onClick={(e) => {
+                handleNextPage();
+                scrollToTop();
+              }}
+            >
               <IconArrowRightCircle className={"h-7 w-7"} />
             </button>
-          </div>
+          ) : (
+            <button
+              onClick={(e) => {
+                handleNextPage();
+                scrollToTop();
+              }}
+            >
+              <IconArrowRightCircle className={"h-7 w-7"} />
+            </button>
+          )}
         </div>
       </div>
-    );
-  } else {
-    return (
-      <div className="relative">
-        {errorInBasket && (
-          <BasketWarningModal
-            setErrorInBasket={setErrorInBasket}
-            msg={errorInBasket.response?.data?.msg}
-          />
-        )}
-        <h2
-          className="text-[2.5rem] text-center my-[2rem] font-bold hidden lg:block "
-          ref={topRef}
-        >
-          <span className="text-[1.2rem]">Our </span>
-          <br />
-          BOOKSTORE
-        </h2>
-
-        <div className="flex">
-          {/* Filters */}
-          <form className="w-[20%]  mr-8 flex flex-col gap-2 ">
-            <h2 className="roboto-regular border-[#778da9] border-b-[1px] pb-2 text-2xl ">
-              Filter by
-            </h2>
-
-            <div className="collapse rounded-none border-[#778da9] border-b-[1px]">
-              <input type="checkbox" />
-              <div className="collapse-title text-[1rem] font-medium">
-                <h2 className="roboto-regular">Publisher</h2>
-              </div>
-              <div className="collapse-content">
-                <div className="flex flex-row">
-                  <input
-                    type="checkbox"
-                    defaultChecked
-                    id="routledge"
-                    className="checkbox h-[1rem] w-[1rem] mr-2 my-auto border-[#778da9] [--chkbg:oklch(var(--a))] [--chkfg:oklch(var(--p))]"
-                  />
-                  <label for="routledge">Routledge</label>
-                </div>
-              </div>
-            </div>
-            <div className="collapse rounded-none border-[#778da9] border-b-[1px]">
-              <input type="checkbox" />
-              <div className="collapse-title text-[1rem] font-medium">
-                <h2 className="roboto-regular">Categories</h2>
-              </div>
-
-              <div className="collapse-content">
-                <p>Categories</p>
-              </div>
-            </div>
-            <div className="collapse rounded-none border-[#778da9] border-b-[1px]">
-              <input type="checkbox" />
-              <div className="collapse-title text-[1rem] font-medium">
-                <h2 className="roboto-regular"> Year</h2>
-              </div>
-
-              <div className="collapse-content">
-                <p> year_from, year_to</p>
-              </div>
-            </div>
-            <div className="collapse rounded-none border-[#778da9] border-b-[1px]">
-              <input type="checkbox" />
-              <div className="collapse-title text-[1rem] font-medium">
-                <h2 className="roboto-regular"> Price</h2>
-              </div>
-
-              <div className="collapse-content">
-                <p> , min_price, max_price,</p>
-              </div>
-            </div>
-          </form>
-          {/* End filters */}
-
-          <div className="gallery-grid justify-center">
-            {" "}
-            {isPending ? (
-              <div className="absolute top-0 bottom-0  w-full h-[80vh] bg-white flex justify-center content-center">
-                <span className="loading loading-spinner loading-lg "></span>
-              </div>
-            ) : data?.books ? (
-              data.books.books.map((book) => (
-                <BookTile
-                  title={book.title}
-                  imageLinks={book.imageLinks}
-                  authors={book.authors}
-                  price={book.price}
-                  key={book._id}
-                  bookId={book._id}
-                  sendToBasket={sendToBasket}
-                />
-              ))
-            ) : (
-              <Navigate to={"/bookstore?page=1"} />
-            )}
-          </div>
-        </div>
-        <div className="flex justify-center items-center  h-9 py-7 ">
-          <div className="flex  gap-4">
-            {currentPage === 1 ? (
-              <button disabled={true} className="hidden">
-                <IconArrowLeftCircle className={"h-7 w-7"} />
-              </button>
-            ) : (
-              <button
-                onClick={(e) => {
-                  handlePrevPage();
-                  scrollToTop();
-                }}
-              >
-                <IconArrowLeftCircle className={"h-7 w-7"} />
-              </button>
-            )}
-
-            <p>Page {currentPage}</p>
-
-            {currentPage === Math.ceil(data?.books?.lengthOfCollection / 12) ? (
-              <button
-                className="hidden"
-                onClick={(e) => {
-                  handleNextPage();
-                  scrollToTop();
-                }}
-              >
-                <IconArrowRightCircle className={"h-7 w-7"} />
-              </button>
-            ) : (
-              <button
-                onClick={(e) => {
-                  handleNextPage();
-                  scrollToTop();
-                }}
-              >
-                <IconArrowRightCircle className={"h-7 w-7"} />
-              </button>
-            )}
-          </div>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
